a4cb96df644671938e2aefb30def833e260f7974
nautilus7 <spyros.n7@gmail.com>
2019-12-31 01:35:08 +0200

    Added files necessary for building and shipping .mo files

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
+import setup_translate
 
 pkg = 'Extensions.GreekStreamTV'
 setup (name = 'enigma2-plugin-extensions-greekstreamtv',
-       version = '3.2',
+       version = '3.4',
        description = 'Watch live stream TV from Greece',
        package_dir = {pkg: 'plugin'},
        packages = [pkg],
        package_data = {pkg: ['channel_background.png', 'depends.sh', 'icons/*.png',
                              'plugin.png', 'stream.xml', 'update.sh', 'xml/*.xml']},
-      )
+       cmdclass = setup_translate.cmdclass,
+)
