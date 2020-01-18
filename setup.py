383cfdb2239bfa1127e9140976510b278c196d82
nautilus7 <spyros.n7@gmail.com>
2020-01-18 20:42:56 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.GreekStreamTV'
 setup (name = 'enigma2-plugin-extensions-greekstreamtv',
-       version = '3.4',
+       version = '3.5',
        description = 'Watch live stream TV from Greece',
        package_dir = {pkg: 'plugin'},
        packages = [pkg],
-       package_data = {pkg: ['channel_background.png', 'depends.sh', 'icons/*.png',
-                             'plugin.png', 'stream.xml', 'update.sh', 'xml/*.xml']},
+       package_data = {pkg: ['icons/*.png', 'plugin.png', 'stream.xml', 'create.sh', 'update.sh', 'xml/*.xml']},
        cmdclass = setup_translate.cmdclass,
 )
